74788af675068b0c0a6c6c56d7076150cbda5c66
Andrew McLellan <andrewmclellan@Andrews-MacBook-Pro.local>
2023-07-17 09:17:57 -0400

    setup router and add search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      Hello, world
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SearchBar />} />
+          {/* <Route path='/details' element={<ImageDetails />}/> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
