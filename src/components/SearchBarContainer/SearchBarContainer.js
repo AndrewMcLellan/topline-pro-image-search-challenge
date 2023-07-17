e4727c317eb43bc25d1688723c5bd152afb1ceec
Andrew McLellan <andrewmclellan@Andrews-MacBook-Pro.local>
2023-07-17 09:53:23 -0400

    adds image indexing component

--- src/components/SearchBarContainer/SearchBarContainer.js
+++ src/components/SearchBarContainer/SearchBarContainer.js
@@ -2,13 +2,14 @@ import { useEffect, useState } from 'react';
 import SearchBarInput from './SearchBarInput/SearchBarInput';
 import './search_bar.scss';
 import { fetchImages } from '../../utils/fetchImages';
+import ImageSearchIndex from '../ImageSearchIndex/ImageSearchIndex';
 
 export const SearchBarContainer = () => {
   const [results, setResults] = useState(null);
 
   const handleSetResults = async (queryString) => {
     const searchResults = await fetchImages({ q: queryString });
-    setResults(searchResults);
+    setResults(searchResults.hits);
   }
 
   return (
@@ -17,6 +18,9 @@ export const SearchBarContainer = () => {
         Search Pixabay for Images! 
       </h2>
       <SearchBarInput handleSetResults={handleSetResults} />
+      {results && (
+        <ImageSearchIndex searchResults={results} />
+      )}
     </div>
   )
 }
